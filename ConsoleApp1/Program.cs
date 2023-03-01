1e6a5c39d6da01959d31c70c4d541b9f9c3ae45c
SergCherkasov <serg.cherkasov@gmail.com>
2023-03-01 09:37:16 +0200

    adding files

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+Method();
+
+void Method()
+{
+    Thread backgroundThread = new Thread(Method);
+
+    Thread.Sleep(1000);
+
+    backgroundThread.IsBackground = true;
+
+    backgroundThread.Start();
+
+    Console.WriteLine($"Start Thread ID: {backgroundThread.ManagedThreadId}");
+
+    Console.WriteLine($"Thread ID: {backgroundThread.ManagedThreadId}\nThreadsCount= {Process.GetCurrentProcess().Threads.Count}\nThreadState:{Thread.CurrentThread.ThreadState}");
+
+    Console.WriteLine($"Finish Thread ID: {backgroundThread.ManagedThreadId}");
+
+    Thread.CurrentThread.Join();
+}
